ae119f0275a76509c15467ebed985aa103110ffd
Giulio Vaccari <giulio09@marcov.it>
2020-12-02 13:49:06 +0100

    Ask to choose chair wifi

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:wifi_iot/wifi_iot.dart';
 
+const String STA_DEFAULT_SSID = "chair";
+const String STA_DEFAULT_PASSWORD = "password";
 
 void main() {
   runApp(MyApp());
@@ -24,15 +27,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -40,22 +34,60 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  Future<void> connetti() async {
+    WiFiForIoTPlugin.connect(STA_DEFAULT_SSID,
+        password: STA_DEFAULT_PASSWORD,
+        joinOnce: true,
+        security: NetworkSecurity.WPA);
+  }
+
   Future<void> sendCommand(forward, backward, left, right, stop) async {
-    var url = 'http://192.168.4.1/data/?sensor_reading={"forward":"'+forward+'",'+'"backward":"'+backward+'","left":"'+left+'","right":"'+right+'","stop":"'+stop+'"}';
-    var result = await http.get(url);
-    print(result);
+    var url = 'http://192.168.4.1/data/?sensor_reading={"forward":"'+forward.toString()+'",'+'"backward":"'+backward.toString()+'","left":"'+left.toString()+'","right":"'+right.toString()+'","stop":"'+stop.toString()+'"}';
+    try {
+      var result = await http.get(url);
+      if (result.body != "OK") {
+        _scaffoldKey.currentState.showSnackBar(
+            SnackBar(
+              content: Text('Error: you are probably not be connected to the chair.'),
+              duration: const Duration(seconds: 5),
+              action: SnackBarAction(
+                label: 'Try to connect',
+                onPressed: () {
+                  connetti();
+                },
+              ),
+            ));
+      }
+    } catch (error) {
+      _scaffoldKey.currentState.showSnackBar(
+          SnackBar(
+            content: Text('Error: ' + error.toString()),
+            duration: const Duration(seconds: 5),
+            action: SnackBarAction(
+              label: 'Try to connect',
+              onPressed: () {
+                connetti();
+              },
+            ),
+          ));
+    }
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // NOTE: Calling this function here would crash the app.
+    connetti();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       backgroundColor: Colors.lightBlue,
+      key: _scaffoldKey,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -102,6 +134,18 @@ class _MyHomePageState extends State<MyHomePage> {
               onPressed: () {
                 sendCommand(0,1,0,0,0);
               },
+            ),
+            SizedBox(height: 50.0),
+            OutlineButton(
+              onPressed: () {
+                sendCommand(0,0,0,0,1);
+              },
+              child: Text(
+                  "Stop",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+              ),
             ),
           ],
         ),
